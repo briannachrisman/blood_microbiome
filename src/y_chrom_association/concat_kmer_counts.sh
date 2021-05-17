ff88696076390e6aed4a9677b6634489e11c30cb
briannac@stanford.edu <briannac@stanford.edu>
2021-05-17 09:21:02 -0700

    Finished creating kmer x sample matrix for y associated kmers

--- src/y_chrom_association/concat_kmer_counts.sh
+++ src/y_chrom_association/concat_kmer_counts.sh
@@ -1,34 +1,30 @@
 #!/bin/sh
 #SBATCH --job-name=concat_kmer_counts
 #SBATCH --partition=dpwall
-#SBATCH --array=6-231%30
+#SBATCH --array=1-232%10
 #SBATCH --output=/scratch/users/briannac/logs/concat_kmer_counts_%a.out
 #SBATCH --error=/scratch/users/briannac/logs/concat_kmer_counts_%a.err
-#SBATCH --time=20:00:00
-#SBATCH --mem=1G
+#SBATCH --time=2:00:00
+#SBATCH --mem=50G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=briannac@stanford.edu
 
 ### file at /home/groups/dpwall/briannac/blood_microbiome/src/y_chrom_association/concat_kmer_counts.sh
 
-for i in 0; do # NOTE: 231 regions to process 
-    N=$((SLURM_ARRAY_TASK_ID+i))
-    N=$(printf "%04g" $N)
-    echo "pasting..."
-    for file in /home/groups/dpwall/briannac/blood_microbiome/intermediate_files/y_chrom_association/*.query_counts.sex_microbes.$N.txt; do
-        sed  '/[*]/d' -i $file
+N=$((SLURM_ARRAY_TASK_ID-1))
+N=$(printf "%04g" $N)
+if [ ! -f $MY_HOME/blood_microbiome/results/y_chrom_association/query_counts.sex_microbes/query_counts.sex_microbes.$N.tsv.gz ]; then
+    mkdir $MY_SCRATCH/tmp/y_chrom_association/$N
+    while read SAMPLE BATCH; do
+        file=/home/groups/dpwall/briannac/blood_microbiome/intermediate_files/y_chrom_association/$SAMPLE/$SAMPLE.query_counts.sex_microbes.$N.txt
+        sed  '/[*]/d' $file > $MY_SCRATCH/tmp/y_chrom_association/$N/$SAMPLE.query_counts.sex_microbes.$N.txt
         echo 'cleaned up' $file
-    done
-    
-    paste /home/groups/dpwall/briannac/blood_microbiome/intermediate_files/y_chrom_association/*.query_counts.sex_microbes.$N.txt > /home/groups/dpwall/briannac/blood_microbiome/intermediate_files/y_chrom_association/query_counts.sex_microbes.$N.tsv
-done
+    done < $MY_HOME/general_data/samples_and_batches.tsv
+    echo "pasting..."
+    paste $MY_SCRATCH/tmp/y_chrom_association/$N/*.txt > $MY_SCRATCH/tmp/y_chrom_association/$N/query_counts.sex_microbes.$N.tsv
+    gzip $MY_SCRATCH/tmp/y_chrom_association/$N/query_counts.sex_microbes.$N.tsv
+    mv $MY_SCRATCH/tmp/y_chrom_association/$N/query_counts.sex_microbes.$N.tsv.gz $MY_HOME/blood_microbiome/results/y_chrom_association/query_counts.sex_microbes/query_counts.sex_microbes.$N.tsv.gz
+    \rm $MY_SCRATCH/tmp/y_chrom_association/$N -r
+fi
 
-cd /home/groups/dpwall/briannac/blood_microbiome/intermediate_files/y_chrom_association
-while read SAMPLE BATCH; do
-    echo $SAMPLE
-    mkdir $SAMPLE
-    for i in {0..230}; do
-        N=$(printf "%04g" $i)
-        mv $SAMPLE.query_counts.sex_microbes.$N.txt $SAMPLE/$SAMPLE.query_counts.sex_microbes.$N.txt
-    done
-done < $MY_HOME/general_data/samples_and_batches.tsv
+
--- src/y_chrom_association/organize_directories.sh
+++ src/y_chrom_association/organize_directories.sh
@@ -12,15 +12,19 @@
 
 cd /home/groups/dpwall/briannac/blood_microbiome/intermediate_files/y_chrom_association
 while read SAMPLE _; do
-    echo "running" $SAMPLE
-    mkdir $SAMPLE
-    mv $SAMPLE.query_counts.sex_microbes.txt.gz $SAMPLE/$SAMPLE.query_counts.sex_microbes.txt.gz
-    mv $SAMPLE.query_counts.sex_microbes.txt $SAMPLE/$SAMPLE.query_counts.sex_microbes.txt
-    mv $SAMPLE.jellyfish.sex_microbes.jf $SAMPLE/$SAMPLE.jellyfish.sex_microbes.jf
-    mv $SAMPLE.jellyfish.sex_microbes.fa $SAMPLE/$SAMPLE.jellyfish.sex_microbes.fa
-    mv $SAMPLE.query_counts.sex_microbes.done $SAMPLE/$SAMPLE.query_counts.sex_microbes.done
-    for i in {0..230}; do
-        N=$(printf "%04g" $i)
-        mv $SAMPLE.query_counts.sex_microbes.$N.txt $SAMPLE/$SAMPLE.query_counts.sex_microbes.$N.txt
-    done
+    if [ -f "$SAMPLE.query_counts.sex_microbes.0230.txt" ]; then
+        echo "running" $SAMPLE
+        mkdir $SAMPLE
+        mv $SAMPLE.query_counts.sex_microbes.txt.gz $SAMPLE/$SAMPLE.query_counts.sex_microbes.txt.gz
+        mv $SAMPLE.query_counts.sex_microbes.txt $SAMPLE/$SAMPLE.query_counts.sex_microbes.txt
+        mv $SAMPLE.jellyfish.sex_microbes.jf $SAMPLE/$SAMPLE.jellyfish.sex_microbes.jf
+        mv $SAMPLE.jellyfish.sex_microbes.fa $SAMPLE/$SAMPLE.jellyfish.sex_microbes.fa
+        mv $SAMPLE.query_counts.sex_microbes.done $SAMPLE/$SAMPLE.query_counts.sex_microbes.done
+        mv $SAMPLE.kraken_classes.tsv $SAMPLE/$SAMPLE.kraken_classes.tsv
+        mv $SAMPLE.y_associated_seqs.fastq $SAMPLE/$SAMPLE.y_associated_seqs.fastq
+        for i in {0..230}; do
+            N=$(printf "%04g" $i)
+            mv $SAMPLE.query_counts.sex_microbes.$N.txt $SAMPLE/$SAMPLE.query_counts.sex_microbes.$N.txt
+        done
+     fi
 done < $MY_HOME/general_data/samples_and_batches.tsv
